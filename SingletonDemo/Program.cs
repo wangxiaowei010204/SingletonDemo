87adc7d0c73442022fcee727411772ea2f388fdc
小伟 汪 <844503316@qq.com>
2020-06-15 18:10:22 +0800

    add singletonDemo

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        private static System.Timers.Timer timer;
+        private static int count = 0;
+        static void Main(string[] args)
+        {
+            timer = new System.Timers.Timer(1000);
+
+            //设置执行一次（false）还是一直执行(true)
+            timer.AutoReset = true;
+            //设置是否执行System.Timers.Timer.Elapsed事件
+            timer.Enabled = true;
+            //绑定Elapsed事件
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(TimerUp);
+            Console.ReadKey();
+        }
+
+        private static void TimerUp(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (count++ == 5)
+            {
+                timer.Stop();
+            }
+
+            TestSingleton.InterlockedMethod();
+
+            TestSingleton.MethodImplOptionsMethod();
+
+            TestSingleton.LockMethod();
+        }
+    }
+}
---
+++ SingletonDemo/TestSingleton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace SingletonDemo
+{
+    public class TestSingleton
+    {
+        private static int _dealing = 0;
+
+        /// <summary>
+        /// 不会等待，直接提示方法进入失败
+        /// </summary>
+        public static void InterlockedMethod()
+        {
+            if (Interlocked.CompareExchange(ref _dealing, 1, 0) == 0)
+            {
+                Console.WriteLine("Interlocked方法进入成功!" + DateTime.Now);
+
+                Thread.Sleep(3000);
+
+                Interlocked.Exchange(ref _dealing, 0);
+            }
+            else
+            {
+                Console.WriteLine("Interlocked方法进入失败!" + DateTime.Now);
+            }
+        }
+
+        /// <summary>
+        /// 方法会等待,必须是静态方法
+        /// </summary>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void MethodImplOptionsMethod()
+        {
+            Console.WriteLine("MethodImplOptionsMethod方法进入成功!" + DateTime.Now);
+
+            Thread.Sleep(3000);
+        }
+
+        private static readonly object SequenceLock = new object();
+
+        /// <summary>
+        /// 方法会等待
+        /// </summary>
+        public static void LockMethod()
+        {
+            lock (SequenceLock)
+            {
+                Console.WriteLine("LockMethod方法进入成功!" + DateTime.Now);
+
+                Thread.Sleep(3000);
+            }
+        }
+
+    }
+}
